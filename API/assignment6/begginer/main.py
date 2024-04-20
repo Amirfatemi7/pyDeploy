926e5e2a9c873a2993e8c60cac5028db03a7bdf3
amirfatemi7 <7.amirfatemi@gmail.com>
2024-04-20 09:33:15 +0330

    bug fixed

--- API/assignment6/begginer/main.py
+++ API/assignment6/begginer/main.py
@@ -10,7 +10,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=True, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
@@ -33,7 +33,7 @@ def get_db():
 
 @app.get("/users")
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(user).filter(User.id == user_id)
+    user = db.query(User).filter(User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="user not found")
     return user
@@ -49,7 +49,7 @@ def add_user(name: str, email: str, db: Session = Depends(get_db)):
 
 @app.delete("/users")
 def remove(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == user_id)
+    user = db.query(User).filter(User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="user not found")
     db.delete(user)
@@ -58,11 +58,13 @@ def remove(user_id: int, db: Session = Depends(get_db)):
 
 @app.put("/users")
 def update_user(user_id: int, name: str, email: str, db: Session = Depends(get_db)):
-    user = User(id = user_id, name= name, email= email)
-    user = db.query(user).filter(User.id == user_id).update(user)
+    # user = User(id = user_id, name= name, email= email)
+    user = db.query(User).filter(User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail="user not found")
-    
+    user.name = name
+    user.email = email
+    db.add(user)
     db.commit()
     db.refresh(user)
     
